2111e81b9e88a06f0199ba6da95f5cc76b98ea1d
Quang <quang@example.com>
2023-11-10 01:38:14 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://aced7941e13b1455bba526494e3f216a-419830311.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-bjcqmjbm.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
