c5bfcf3faf6894642b6395446b0aa8e3e70a38ce
Quang <quang@example.com>
2023-11-13 05:51:21 +0700

    update new link backend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a3feff7c680ff457cae7610165799167-399403798.us-east-1.elb.amazonaws.com'
 };
 
 /*
