c576b7f719cc0136347308a98d4263cf3a413467
Quang <quang@example.com>
2023-11-13 19:51:15 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a3feff7c680ff457cae7610165799167-399403798.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a992a3f46d8684341be1c7acd235c565-829279486.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
